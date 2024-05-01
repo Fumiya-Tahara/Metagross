fa65f17de32170b709bbd9b5a0f863f3eaaf6ba0
kurumayusamugariko <junmomo6780@gmail.com>
2024-05-01 22:43:56 +0900

    階段の上り方

--- test.js
+++ test.js
@@ -3,20 +3,17 @@ const readline = require('readline').createInterface({
   output: process.stdout
 });
 
-readline.question('足場の高さを5つ、カンマ区切りで入力してください: ', input => {
-  let h = input.split(',').map(Number);
-  let dp = new Array(h.length).fill(Infinity);
+readline.question('階段の段数を入力してください: ', input => {
+  let N = Number(input);
+  let dp = new Array(N + 1).fill(0);
 
-  dp[0] = 0;
+  dp[0] = 1; // 0段目
+  dp[1] = 1; // 1段目
 
-  for (let i = 1; i < h.length; i++) {
-    if (i == 1) {
-      dp[i] = Math.abs(h[i] - h[i-1]);
-    } else {
-      dp[i] = Math.min(dp[i-1] + Math.abs(h[i] - h[i-1]), dp[i-2] + Math.abs(h[i] - h[i-2]));
-    }
+  for (let n = 2; n <= N; n++) {
+    dp[n] = dp[n-1] + dp[n-2];
   }
 
-  console.log(dp[h.length - 1]);
+  console.log(dp[N]);
   readline.close();
 });
