a4f90b705805217cdd679c43323579c01721163b
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-25 08:57:45 +0900

    Add 4-4-3

---
+++ chap4/chap4-4/4-4-3/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	maxN   = 500000
+	maxVal = 1000000000
+)
+
+var (
+	sc     = bufio.NewScanner(os.Stdin)
+	buffer = make([]byte, maxN*12)
+)
+
+func scanStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func scanInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func scanLineStr() []string {
+	s := scanStr()
+
+	return strings.Split(s, " ")
+}
+
+func scanLineInt() []int {
+	s := scanStr()
+	strList := strings.Split(s, " ")
+
+	slice := make([]int, len(strList))
+	for i, v := range strList {
+		num, _ := strconv.Atoi(string(v))
+		slice[i] = num
+	}
+
+	return slice
+}
+
+func main() {
+	sc.Buffer(buffer, maxVal)
+	sc.Split(bufio.ScanWords)
+
+	N := scanInt()
+	f := make([]int, N+1)
+
+	for i := 1; i <= N+1; i++ {
+		for j := i; j <= N+1; j += i {
+			f[j]++
+		}
+	}
+
+	fmt.Println(f)
+
+}
