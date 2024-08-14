9a65f49bf271308b24731286929dc1eb7d20106b
kurumayusamugariko <junmomo6780@gmail.com>
2024-08-15 00:01:09 +0900

    4-6-2

--- chap4/chap4-6/4-6-2/4-6-2.js
+++ chap4/chap4-6/4-6-2/4-6-2.js
@@ -1,5 +1,6 @@
 function modpow(a, b, m) {
-  let p = a, ans = 1n;
+  let p = a,
+    ans = 1n;
   for (let i = 0n; i < 30; i++) {
     if ((b & (1n << i)) != 0) {
       ans *= p;
@@ -11,38 +12,36 @@ function modpow(a, b, m) {
   return ans;
 }
 function division(a, b, m) {
-  return (a * modpow(b, m-2n, m)) % m;
+  return (a * modpow(b, m - 2n, m)) % m;
 }
 
-function main(input) {
-  input = input.trim().split('\n');
-  let [x,y] = input[0].split(' ').map(val => BigInt(val));
+function Main(input) {
+  input = input.trim().split("\n");
+  let [x, y] = input[0].split(" ").map((val) => BigInt(val));
 
   let mod = 1000000007n;
-  if (2n*y-x<0 || 2n*x-y<0) {
-    return console.log(0); 
+  if (2n * y - x < 0 || 2n * x - y < 0) {
+    return console.log(0);
   }
-  if ((2n*y-x)%3n!=0 || (2n*x-y)%3n!=0) {
-    return console.log(0); 
+  if ((2n * y - x) % 3n != 0 || (2n * x - y) % 3n != 0) {
+    return console.log(0);
   }
   let bunshi = 1n;
   let bunbo = 1n;
-  let a = (2n*y-x) / 3n;
-  let b = (2n*x-y) / 3n;
-  for (let i = 1n; i <= a+b; i++) {
+  let a = (2n * y - x) / 3n;
+  let b = (2n * x - y) / 3n;
+  for (let i = 1n; i <= a + b; i++) {
     bunshi *= i;
     bunshi %= mod;
   }
   for (let i = 1n; i <= a; i++) {
-    bunbo *=i;
+    bunbo *= i;
     bunbo %= mod;
   }
   for (let i = 1n; i <= b; i++) {
-    bunbo *=i;
+    bunbo *= i;
     bunbo %= mod;
   }
   console.log(division(bunshi, bunbo, mod).toString());
-  
-  
 }
-main(require("fs").readFileSync("/dev/stdin", "utf8"));
+Main(require("fs").readFileSync("/dev/stdin", "utf8"));
