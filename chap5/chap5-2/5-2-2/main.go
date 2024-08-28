7501d10935bdc45fc28fb1b9a788e9771a78048f
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-08-28 23:11:52 +0900

    Add 規則性を考える

---
+++ chap5/chap5-2/5-2-2/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	maxN   = 1000000
+	maxVal = 1000000
+)
+
+var (
+	sc     = bufio.NewScanner(os.Stdin)
+	buffer = make([]byte, maxN*12)
+)
+
+func scanStr() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func scanInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func scanInt64() int64 {
+	sc.Scan()
+	i, err := strconv.ParseInt(sc.Text(), 10, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func scanLineStr() []string {
+	s := scanStr()
+
+	return strings.Split(s, " ")
+}
+
+func scanLineInt() []int {
+	s := scanStr()
+	strList := strings.Split(s, " ")
+
+	slice := make([]int, len(strList))
+	for i, v := range strList {
+		num, _ := strconv.Atoi(string(v))
+		slice[i] = num
+	}
+
+	return slice
+}
+
+func main() {
+	sc.Buffer(buffer, maxVal)
+
+	N := scanInt()
+
+	var flag bool
+	for i := 1; i <= 60; i++ {
+		if N == ((1 << i) - 1) {
+			flag = true
+		}
+	}
+
+	if flag {
+		fmt.Println("Second")
+		return
+	}
+
+	fmt.Println("First")
+}
