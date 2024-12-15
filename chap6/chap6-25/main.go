5e45385f856935eacb7a99274c1c4528be800f25
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-12-15 17:15:56 +0900

    Fix 問題26

--- chap6/chap6-25/main.go
+++ chap6/chap6-25/main.go
@@ -12,18 +12,19 @@ var sc = bufio.NewScanner(os.Stdin)
 func scan() int {
 	sc.Scan()
 	n, _ := strconv.Atoi(sc.Text())
+
 	return n
 }
 
-func dfs(g *[][]int, visited *[]bool, dp *[]int, pos int) {
-	(*visited)[pos] = true
-	(*dp)[pos] = 1
-	for _, v := range (*g)[pos] {
-		if (*visited)[v] {
-			continue
+func dfs(current int, g *[][]int, visited *[]bool, dp *[]int) {
+	(*visited)[current] = true
+	(*dp)[current] = 1
+
+	for _, v := range (*g)[current] {
+		if !(*visited)[v] {
+			dfs(v, g, visited, dp)
+			(*dp)[current] += (*dp)[v]
 		}
-		dfs(g, visited, dp, v)
-		(*dp)[pos] += (*dp)[v]
 	}
 }
 
@@ -31,23 +32,24 @@ func main() {
 	sc.Split(bufio.ScanWords)
 
 	n := scan()
+
 	g := make([][]int, n+1)
-	for i := 0; i < n; i++ {
+	for i := 1; i < n; i++ {
 		a := scan()
 		b := scan()
 		g[a] = append(g[a], b)
 		g[b] = append(g[b], a)
 	}
 
-	dp := make([]int, n+1)
 	visited := make([]bool, n+1)
+	dp := make([]int, n+1)
 
-	dfs(&g, &visited, &dp, 1)
+	dfs(1, &g, &visited, &dp)
 
-	ans := 0
-	for i := 2; i <= n; i++ {
-		ans += dp[i] * (n - dp[i])
+	var answer int
+	for i := 1; i < n+1; i++ {
+		answer += dp[i] * (n - dp[i])
 	}
 
-	fmt.Println(ans)
+	fmt.Println(answer)
 }
